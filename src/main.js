bdcd0c7b57b55176ba176826ae4e9e8a6dd8b1e3
haiou1314 <1871943511@qq.com>
2022-06-30 20:28:23 +0800

    移动端导航切换高亮案例

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import App from './10_附加练习_3.购物车.vue'
+import App from './11_移动端-导航切换效果.vue'
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 
 Vue.config.productionTip = false
