462ec490faf6b9bc5cb4b869192039f768bf4c48
haiou1314 <1871943511@qq.com>
2022-06-30 19:09:25 +0800

    v-if v-show的区别

---
+++ src/02_vue指令_v-if和v-show的使用.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <p v-show="isshow">v-show的显示方式是利用css属性html标签不会被删除用的是display:none属性</p>
+    <button @click="isshow = !isshow">按钮</button>
+    <p v-if="isshow1">v-if的显示方式是删除html标签，可以用多种条件来限制类似于if else语句</p>
+    <button @click="isshow1 = !isshow1">按钮</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      isshow: true,
+      isshow1: true,
+    }
+  },
+}
+</script>
+
+<style></style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import App from './01_vue指令_v-html和v-text的区别.vue'
+import App from './02_vue指令_v-if和v-show的使用.vue'
 
 Vue.config.productionTip = false
 
