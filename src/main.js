cd73067f2d59eb7a5f31e0ef1efb0fcd3bdaf66e
haiou1314 <1871943511@qq.com>
2022-07-01 09:47:57 +0800

    学生信息管理

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import App from './11_移动端-导航切换效果.vue'
+import App from './12_学生信息管理.vue'
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 
 Vue.config.productionTip = false
